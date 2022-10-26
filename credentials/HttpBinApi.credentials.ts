c2e18eaec7959b751274c30eda75f7177eeecdd6
redanthrax <redanthraxx@gmail.com>
2022-10-25 21:37:55 -0700

    initial

--- credentials/HttpBinApi.credentials.ts
+++ credentials/HttpBinApi.credentials.ts
@@ -8,7 +8,7 @@ import {
 export class HttpBinApi implements ICredentialType {
 	name = 'httpbinApi';
 	displayName = 'HttpBin API';
-	documentationUrl = '<your-docs-url>';
+	documentationUrl = 'https://github.com/redanthrax/tacticalrmm-node';
 	properties: INodeProperties[] = [
 		{
 			displayName: 'Token',
@@ -28,14 +28,14 @@ export class HttpBinApi implements ICredentialType {
 	// stating how this credential is injected as part of the request
 	// An example is the Http Request node that can make generic calls
 	// reusing this credential
-	authenticate = {
+	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			headers: {
 				Authorization: '={{"Bearer " + $credentials.token}}',
 			},
 		},
-	} as IAuthenticateGeneric;
+	};
 
 	// The block below tells how this credential can be tested
 	test: ICredentialTestRequest = {
