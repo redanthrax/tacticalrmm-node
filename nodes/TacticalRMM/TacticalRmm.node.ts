ef797b1f0eb2c727aa2389bb5b14439f13973091
redanthrax <gary@4c.gg>
2025-11-09 21:11:25 -0800

    fix: bring code up to standard node style

--- nodes/TacticalRMM/TacticalRmm.node.ts
+++ nodes/TacticalRMM/TacticalRmm.node.ts
@@ -1,80 +1,109 @@
 import * as agent from './actions/agent';
-import * as client from './actions/client';
 import * as alert from './actions/alert';
+import * as client from './actions/client';
 import * as software from './actions/software';
 
 import {
 	IExecuteFunctions,
+	ILoadOptionsFunctions,
+	INodeExecutionData,
+	INodePropertyOptions,
 	INodeType,
 	INodeTypeDescription,
+	LoggerProxy as Logger,
 	NodeConnectionType,
 } from 'n8n-workflow';
 
-import { loadOptions } from './methods';
-import { router } from './actions/router';
-
 export class TacticalRmm implements INodeType {
-		description: INodeTypeDescription = {
-				displayName: 'Tactical RMM',
-				name: 'tacticalRmm',
-				icon: 'file:trmm.svg',
-			group: ['transform'],
-				version: 1,
-				subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-				description: 'Get data from the TacticalRMM API',
-				defaults: {
-						name: 'Tactical RMM',
-				},
-				inputs: [NodeConnectionType.Main],
-				outputs: [NodeConnectionType.Main],
-				credentials: [
-						{
-								name: 'tacticalRMMApi',
-								required: true,
-						},
+	description: INodeTypeDescription = {
+		displayName: 'Tactical RMM',
+		name: 'tacticalRmm',
+		icon: 'file:trmm.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Get data from the TacticalRMM API',
+		documentationUrl: 'https://github.com/redanthrax/tacticalrmm-node',
+		defaults: {
+			name: 'Tactical RMM',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		usableAsTool: true,
+		credentials: [
+			{
+				name: 'tacticalRMMApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Agent',
+						value: 'agent',
+					},
+					{
+						name: 'Alert',
+						value: 'alert',
+					},
+					{
+						name: 'Client',
+						value: 'client',
+					},
+					{
+						name: 'Software',
+						value: 'software',
+					},
 				],
-				requestDefaults: {
-						headers: {
-								Accept: 'application/json',
-								'Content-Type': 'application/json',
-						},
-				},
-				properties: [
-						{
-								displayName: 'Resource',
-								name: 'resource',
-								type: 'options',
-								noDataExpression: true,
-								options: [
-										{
-												name: 'Agent',
-												value: 'agent',
-										},
-										{
-												name: 'Alert',
-												value: 'alert',
-										},
-										{
-												name: 'Client',
-												value: 'client',
-										},
-										{
-												name: 'Software',
-												value: 'software',
-										},
-								],
-								default: 'agent',
-						},
-						...agent.description,
-						...alert.description,
-						...client.description,
-						...software.description,
-				],
-		};
+				default: 'agent',
+			},
+			...agent.description,
+			...alert.description,
+			...client.description,
+			...software.description,
+		],
+	};
 
-		methods = { loadOptions };
+	methods = {
+		loadOptions: {
+			getClients: async function(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				Logger.debug('Loading clients for TacticalRMM node', { node: this.getNode().name });
+				const { apiRequest } = await import('./transport');
+				try {
+					const requestMethod = 'GET';
+					const endpoint = '/clients/';
+					const body = {};
+					const qs = {};
 
-		async execute(this: IExecuteFunctions) {
-				return await router.call(this);
-		}
+					const responseData = await apiRequest.call(this, requestMethod, endpoint, body, qs);
+
+					const options: INodePropertyOptions[] = [];
+					if (Array.isArray(responseData)) {
+						for (const client of responseData) {
+							if (client.id && client.name) {
+								options.push({
+									name: client.name,
+									value: client.id.toString(),
+									description: client.name || '',
+								});
+							}
+						}
+					}
+					return options.sort((a, b) => a.name.localeCompare(b.name));
+				} catch (error) {
+					return [];
+				}
+			},
+		},
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const { router } = await import('./actions/router');
+		return await router.call(this);
+	}
 }
--- nodes/TacticalRMM/actions/router.ts
+++ nodes/TacticalRMM/actions/router.ts
@@ -1,59 +1,61 @@
 import { IExecuteFunctions } from 'n8n-workflow';
 
 import {
-		IDataObject,
-		INodeExecutionData,
+	IDataObject,
+	INodeExecutionData,
 } from 'n8n-workflow';
 
-import * as agent from './agent';
-import * as alert from './alert';
-import * as client from './client';
-import * as software from './software';
 import { TacticalRMM } from './Interfaces';
 
 export async function router(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const items = this.getInputData();
-		const operationResult: INodeExecutionData[] = [];
-		let responseData: IDataObject | IDataObject[] = [];
+	const items = this.getInputData();
+	const operationResult: INodeExecutionData[] = [];
+	let responseData: IDataObject | IDataObject[] = [];
 
-		for (let i = 0; i < items.length; i++) {
-				const resource = this.getNodeParameter<TacticalRMM>('resource', i);
-				const operation = this.getNodeParameter('operation', i);
+	for (let i = 0; i < items.length; i++) {
+		const resource = this.getNodeParameter<TacticalRMM>('resource', i);
+		const operation = this.getNodeParameter('operation', i);
 
-				const tacticalrmm = {
-						resource,
-						operation,
-				} as TacticalRMM;
+		const tacticalrmm = {
+			resource,
+			operation,
+		} as TacticalRMM;
 
-				try {
-						switch(tacticalrmm.resource) {
-								case 'agent':
-										responseData = await agent[tacticalrmm.operation].execute.call(this, i);
-										break;
-								case 'alert':
-										responseData = await alert[tacticalrmm.operation].execute.call(this, i);
-										break;
-								case 'client':
-										responseData = await client[tacticalrmm.operation].execute.call(this, i);
-										break;
-								case 'software':
-										responseData = await software[tacticalrmm.operation].execute.call(this, i);
-										break;
-								default:
-										break;
-						}
+		try {
+			let resourceModule;
 
-						const executionData = this.helpers.returnJsonArray(responseData);
-						operationResult.push(...executionData);
-				} catch (err) {
-						if (this.continueOnFail()) {
-								operationResult.push({ json: this.getInputData(i)[0].json, error: err });
-						} else {
-								if (err.context) err.context.itemIndex = i;
-								throw err;
-						}
-				}
+			switch (tacticalrmm.resource) {
+				case 'agent':
+					resourceModule = await import('./agent');
+					break;
+				case 'alert':
+					resourceModule = await import('./alert');
+					break;
+				case 'client':
+					resourceModule = await import('./client');
+					break;
+				case 'software':
+					resourceModule = await import('./software');
+					break;
+				default:
+					break;
+			}
+
+			if (resourceModule) {
+				responseData = await (resourceModule as any)[tacticalrmm.operation].execute.call(this, i);
+			}
+
+			const executionData = this.helpers.returnJsonArray(responseData);
+			operationResult.push(...executionData);
+		} catch (err) {
+			if (this.continueOnFail()) {
+				operationResult.push({ json: this.getInputData(i)[0].json, error: err });
+			} else {
+				if (err.context) err.context.itemIndex = i;
+				throw err;
+			}
 		}
+	}
 
-		return [operationResult];
+	return [operationResult];
 }
